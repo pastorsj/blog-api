93de4689617e6c8aaba92a34b3d1f73545ceb4ff
pastorsj <pastorsj@rose-hulman.edu>
2018-02-19 21:15:09 -0500

    Updated docker files, added setup file

---
+++ setup.16.04.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+# Complete docker installation
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# Pull docker repositories
+docker pull node
+docker pull mongo
+docker pull redis
+docker pull nginx
+
