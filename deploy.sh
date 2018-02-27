ff386ff76dd010ecdc363e263db9083b4871d6dc
pastorsj <pastorsj@rose-hulman.edu>
2018-02-26 20:30:10 -0500

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker run --rm --name letsencrypt -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -v "/usr/share/nginx/html:/usr/share/nginx/html" certbot/certbot:latest renew --quiet
 docker stop $(docker ps -q)
+docker-compose rm blog-api
 docker-compose -f docker-compose.prod.yml up --build -d
