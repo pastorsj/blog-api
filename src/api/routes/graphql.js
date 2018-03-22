e9f4492643528e02141b2c4c36c0fc318603e523
pastorsj <sampastoriza@yahoo.com>
2018-03-22 19:58:29 -0400

    Only allow graphql for authenticated users

--- src/api/routes/graphql.js
+++ src/api/routes/graphql.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import RootSchema from '../graphql/root.schema';
+import { auth } from '../../config/jwt.config';
 
 const router = express.Router();
 
 // Availible via the base_url/articles route
-router.use('/', graphqlHTTP({
+router.use('/', auth, graphqlHTTP({
     schema: RootSchema,
     graphiql: true,
     formatError(err) {
