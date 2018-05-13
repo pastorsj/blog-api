7eaf9dde2e96795d9e74472d2fc19a76f4b0a633
pastorsj <sampastoriza@yahoo.com>
2018-05-13 10:00:56 -0400

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ docker stop $(docker ps -a -q)
 docker-compose -f docker-compose.prod.yml rm -f blog-api
 docker-compose -f docker-compose.prod.yml build --no-cache blog-api
 docker-compose -f docker-compose.prod.yml up --force-recreate -d
+docker rmi $(docker images -a | grep "^<none>" | awk '{print $3}')
